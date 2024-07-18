9747a8ff3c6f809c960ce121ba48150fb99e1f75
F-Shams <fshamsf67@gmail.com>
2024-07-18 15:16:15 +0200

    my python projects: making new get name and age FastAPI

--- main.py
+++ main.py
@@ -16,13 +16,16 @@ def page_one():
     return {f'massage': 'ok'}
 
 
-@app.get('/first-page/{name}')
-def id_pv(name: str):
-    return {f'massage': '{name}'}
+@app.get('/first-page/{ids}')
+def id_pv(ids: str):
+    return {'massage': f'{ids}'}
 
 
-@app.get('/first-name/ {name} / {age}')
-def info(name:str, age: int, number: Optional[int] = None):
-    return {'name': f'{name}', 'age': f'{age}', 'number': f'{number}'}
+@app.get('/first-page/{name}/{age}')
+def info(name: str, age: int, number: Optional[int] = None):
+    return {'name': name,
+            'age': age,
+            'number': number
+            }
 
 
