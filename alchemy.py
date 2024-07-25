bb0f109e51dd2f688a0bb67ebc5fd7d1908dff39
F-Shams <fshamsf67@gmail.com>
2024-07-25 15:02:09 +0200

    alchemy.py : Making an example for my table

--- alchemy.py
+++ alchemy.py
@@ -7,7 +7,7 @@ db_url = 'sqlite:///db.sqlite'
 engine = create_engine(db_url)
 
 
-class Animals(Base):
+class Animal(Base):
     __tablename__ = 'table1'
     id = Column(Integer, primary_key=True)
     name = Column(String)
@@ -16,9 +16,18 @@ class Animals(Base):
 
 Base.metadata.create_all(engine)
 
+animal0 = Animal(name='dog', color='brown')
+animal1 = Animal(name='cat', color='white')
+animal2 = Animal(name='donkey', color='black')
+
 
 Session = sessionmaker(bind=engine)
 session = Session
-session.commit()
+session.add(animal0)
+session.add_all([animal1, animal2])
+
+to print animals
+animals = session.
+
 
 
